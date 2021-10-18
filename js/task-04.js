1b5f3eacf7655bb5c921e43be7b8f4899049fd5b
Andrii Miroshnichenko <andy.miroshnichenko@gmail.com>
2021-10-18 14:28:10 +0300

    Done task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,11 @@
-const counterValue = 0;
-const score = document.querySelector('#counter');
+const btnDecrementEl = document.querySelector('button[data-action="decrement"]');
+const btnIncrementEl=document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+let counterValue = 0;
+
+const decrement = () => valueEl.textContent = counterValue -= 1;
+const increment = () => valueEl.textContent = counterValue += 1;
+
+btnDecrementEl.addEventListener('click', decrement);
+btnIncrementEl.addEventListener('click', increment);
