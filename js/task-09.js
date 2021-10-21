6f0f1823b0bfbbd63bd6a91d9193da53bcd20763
Andrii Miroshnichenko <andy.miroshnichenko@gmail.com>
2021-10-21 16:38:00 +0300

    Done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+// 1. Получить из DOM классы ".color", ".change-color".
+// 2. Повесить слушателя на button при событии click.
+// 3. Создать функцию, которая будет менять у body backgroundColor
+//    и передавать название цвета в span.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const span = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', () => {
+  let color = getRandomHexColor();
+  span.textContent = color;
+  document.body.style.backgroundColor = color;
+})
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,9 @@
+// 1. Получить из DOM классы ".color", ".change-color".
+// 2. Повесить слушателя на button при событии click.
+// 3. Создать функцию, которая будет менять у body backgroundColor
+//    и передавать название цвета в span.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
