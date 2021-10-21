4ca5fd3fad50d0244d886c4f1d787f8c3c067df5
Andrii Miroshnichenko <andy.miroshnichenko@gmail.com>
2021-10-21 12:58:22 +0300

    Done task-07

---
+++ js/task-07.js
@@ -0,0 +1,18 @@
+// 1. Получить из DOM классы "#font-size-control" и "#text".
+// 2. Повесить слушателя на input при событии input.
+// 3.
+// 4.
+
+const controlFont = document.querySelector("#font-size-control");
+const textValue = document.querySelector("#text");
+
+const handleInput = () => {
+    textValue.style.fontSize = `${controlFont.value}px`;
+  };
+
+  controlFont.addEventListener("input", handleInput);
+
+
+
+
+
